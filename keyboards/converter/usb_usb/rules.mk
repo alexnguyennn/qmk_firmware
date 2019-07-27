0a04946346e2a0651e4f3966bba50ca3388001d0
Alex <alex.nguyen@rokt.com>
2019-07-27 18:47:20 +1000

    add usb converter configuration

--- keyboards/converter/usb_usb/rules.mk
+++ keyboards/converter/usb_usb/rules.mk
@@ -1,11 +1,11 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+#BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE  = yes  # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = yes  # Audio control and System control(+450)
+#CONSOLE_ENABLE   = yes  # Console for debug(+400)
+#COMMAND_ENABLE   = yes  # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
@@ -16,3 +16,7 @@ CUSTOM_MATRIX = yes
 SRC += custom_matrix.cpp
 
 DEFAULT_FOLDER = converter/usb_usb/hasu
+
+LAYOUTS = fullsize_ansi fullsize_iso
+
+DEBOUNCE_TYPE = sym_g
